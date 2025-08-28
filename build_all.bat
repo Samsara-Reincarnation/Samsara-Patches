e7696bf69bf39ef44d27c5a3e7d1e45337e09594
cubebert-github <cubebertoffical@gmail.com>
2025-08-27 17:45:14 -0700

    Added a batch file for compiling every patch
    -Added a batch file for compiling every patch, along with updating the pk3 version

---
+++ build_all.bat
@@ -0,0 +1,24 @@
+@echo off
+
+REM Set the filename for the pk3 files
+SET pk3Name=SamsaraPatch-
+SET pk3Version=_v2.1.3
+
+REM Go through each folder in the directory
+for /d %%a in (*) do (
+
+REM Delete the pk3 file if it exists
+if exist %pk3Name%%%a%pk3Version%.pk3 del %pk3Name%%%a%pk3Version%.pk3
+
+REM Move to the specified directory
+cd %%a
+
+REM Build the directory and compile it to the root folder
+..\_tools\7za.exe a -r -ssw -tzip -mx9 ..\%pk3Name%%%a%pk3Version%.pk3
+
+REM Move back to the root folder for the loop
+cd ..\)
+
+REM The _tools folder shouldn't be put into a pk3 file, so delete it
+del %pk3Name%_tools%pk3Version%.pk3
+exit
